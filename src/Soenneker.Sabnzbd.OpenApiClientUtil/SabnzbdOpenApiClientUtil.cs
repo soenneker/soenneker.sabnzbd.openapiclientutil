86b73d81fe77bec0ddf02b8976b26e00b5f928dc
soenneker <jake@soenneker.com>
2026-09-03 22:01:44 -0500

    Add interface inheritdoc references

--- src/Soenneker.Sabnzbd.OpenApiClientUtil/SabnzbdOpenApiClientUtil.cs
+++ src/Soenneker.Sabnzbd.OpenApiClientUtil/SabnzbdOpenApiClientUtil.cs
@@ -14,6 +14,7 @@ using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Sabnzbd.OpenApiClientUtil;
 
+/// <inheritdoc cref="ISabnzbdOpenApiClientUtil" />
 public sealed class SabnzbdOpenApiClientUtil : ISabnzbdOpenApiClientUtil
 {
     private readonly AsyncSingleton<SabnzbdOpenApiClient> _client;
